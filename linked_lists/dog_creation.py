cd203128cfd79a02212c02eac2bff7e330501019
Krystal Flores <krystal.flores@gmail.com>
2017-02-23 14:05:35 -0800

    Creating different functions to use with AllAboutArya

--- linked_lists/dog_creation.py
+++ linked_lists/dog_creation.py
@@ -1,14 +1,57 @@
+#!/usr/bin/python
+
 import dog_related_classes
 
-arya = dog_related_classes.Dog('wags her tail')
-poe = dog_related_classes.Dog('pees a lot')
-daisy = dog_related_classes.Dog('is super big and adorable')
+#creating descriptions of arya
+description_one = dog_related_classes.AryaTheDog('wags her tail')
+description_two = dog_related_classes.AryaTheDog('loves peanute butter')
+description_three = dog_related_classes.AryaTheDog('is super tiny and adorable')
+description_four = dog_related_classes.AryaTheDog('furiously awesome')
 
-print(arya.value)
+#instantiating the class of AllAboutArya and appending descriptions
+descriptions_of_arya = dog_related_classes.AllAboutArya(description_one)
+descriptions_of_arya.append(description_two)
+descriptions_of_arya.append(description_three)
+descriptions_of_arya.append(description_four)
 
+print('Initial description_values:')
+print('description of arya start description_value: ' + descriptions_of_arya.start.description_value)
+print('description of arya next description_value: ' + descriptions_of_arya.start.next.description_value)
+print('description of arya next next description_value: ' + descriptions_of_arya.start.next.next.description_value)
+print('descriptions of arya, description in first position: ' + str(descriptions_of_arya.get_dog_description_position(1).description_value))
+print('descriptions of arya, description in second position: ' + str(descriptions_of_arya.get_dog_description_position(2).description_value))
+print('descriptions of arya, description in third position: ' + str(descriptions_of_arya.get_dog_description_position(3).description_value))
+print('descriptions of arya, description in fourth position: ' + str(descriptions_of_arya.get_dog_description_position(4).description_value))
 
-the_puppy_brigade = dog_related_classes.LinkedDogs(arya)
-the_puppy_brigade.append(poe)
-the_puppy_brigade.append(daisy)
+#insert new descriptions into AllAboutArya
+description_five = dog_related_classes.AryaTheDog('has a brother named Poe')
+description_six = dog_related_classes.AryaTheDog('lives in DTLA')
 
-#print(the_puppy_brigade)
+descriptions_of_arya.insert_new_description(description_five,1)
+descriptions_of_arya.insert_new_description(description_six,3)
+
+print('\n')
+print('Description_values after insertion:')
+print('description of arya start description_value: ' + descriptions_of_arya.start.description_value)
+print('description of arya next description_value: ' + descriptions_of_arya.start.next.description_value)
+print('description of arya next next description_value: ' + descriptions_of_arya.start.next.next.description_value)
+print('descriptions of arya, description in first position: ' + str(descriptions_of_arya.get_dog_description_position(1).description_value))
+print('descriptions of arya, description in second position: ' + str(descriptions_of_arya.get_dog_description_position(2).description_value))
+print('descriptions of arya, description in third position: ' + str(descriptions_of_arya.get_dog_description_position(3).description_value))
+print('descriptions of arya, description in fourth position: ' + str(descriptions_of_arya.get_dog_description_position(4).description_value))
+print('descriptions of arya, description in fifth position: ' + str(descriptions_of_arya.get_dog_description_position(5).description_value))
+print('descriptions of arya, description in sixth position: ' + str(descriptions_of_arya.get_dog_description_position(6).description_value))
+
+#delete descriptions from AllAboutArya
+descriptions_of_arya.delete_old_description(1)
+descriptions_of_arya.delete_old_description(4)
+
+print('\n')
+print('Description values after deletion:')
+print('description of arya start description_value: ' + descriptions_of_arya.start.description_value)
+print('description of arya next description_value: ' + descriptions_of_arya.start.next.description_value)
+print('description of arya next next description_value: ' + descriptions_of_arya.start.next.next.description_value)
+print('descriptions of arya, description in first position: ' + str(descriptions_of_arya.get_dog_description_position(1).description_value))
+print('descriptions of arya, description in second position: ' + str(descriptions_of_arya.get_dog_description_position(2).description_value))
+print('descriptions of arya, description in third position: ' + str(descriptions_of_arya.get_dog_description_position(3).description_value))
+print('descriptions of arya, description in fourth position: ' + str(descriptions_of_arya.get_dog_description_position(4).description_value))
--- linked_lists/dog_related_classes.py
+++ linked_lists/dog_related_classes.py
@@ -1,27 +1,65 @@
-class Dog(object):
+#!/usr/bin/python
+
+class AryaTheDog(object):
     """ This class creates a dog """
-    def __init__(self, value):
-      self.value = value
+    def __init__(self, description_value):
+      self.description_value = description_value
       self.next = None
 
 
 
-class LinkedDogs(object):
-    """ This class creates a linked list of dogs """
-    def __init__(self, link=None):
-      self.link = link
+class AllAboutArya(object):
+    """ This class creates a started list of dogs """
+    def __init__(self, start=None):
+      self.start = start
+      
 
-    def append(self, new_dog):
-        current = self.link
-        if self.link:
-            while current.next:
-                current = current.next
-            current.next = new_dog
+    def append(self, new_description):
+        current_description = self.start
+        if self.start:
+            while current_description.next:
+                current_description = current_description.next
+            current_description.next = new_description
+            #print('current_description.next: ' + str(new_description))
         else:
-          self.link = new_dog
+          self.start = new_description
+          #print('self.start: ' + str(new_description))
 
-
-
-
-
+    def get_dog_description_position(self,position_of_dog_description):
+        description_counter = 1
+        current_description = self.start
+        if position_of_dog_description < 1:
+            return None
+        while current_description and description_counter <= position_of_dog_description:
+            if description_counter == position_of_dog_description:
+                return current_description
+            current_description = current_description.next
+            description_counter += 1
+        return None
+    
+    def insert_new_description(self,new_description,position_of_dog_description):
+        description_counter = 1
+        current_description = self.start
+        if position_of_dog_description > 1:
+            while current_description and description_counter < position_of_dog_description:
+                    if description_counter == position_of_dog_description - 1:
+                        new_description.next = current_description.next
+                        current_description.next = new_description
+                    current_description = current_description.next
+                    description_counter += 1
+        elif position_of_dog_description == 1:
+            new_description.next = self.start
+            self.start = new_description
+            
+    def delete_old_description(self,description_value):      
+        current_description = self.start
+        previous_description = None
+        while current_description.description_value != description_value and current_description.next:
+            previous_description = current_description
+            current_description = current_description.next 
+        if current_description.description_value == description_value:
+            if previous_description:
+                previous_description.next == current_description.next
+            else:
+                self.start = current_description.next
 
